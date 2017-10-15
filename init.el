c8f3d712328cef2ebd8e82ff6ad24c490939e946
LS8 <lennartschoettker@hotmail.com>
2017-10-15 09:03:54 +0200

    use C-n and C-p to cycle company mode suggestions

--- init.el
+++ init.el
@@ -272,3 +272,9 @@
   (delete-trailing-whitespace)
   (indent-region (point-min) (point-max)))
 
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous))
+
