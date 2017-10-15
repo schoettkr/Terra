bb64b245ea7750da5e1ad262b2f52116c5a9c852
LS8 <lennartschoettker@hotmail.com>
2017-10-15 22:12:11 +0200

    set tabwidth for typescript

--- init.el
+++ init.el
@@ -311,10 +311,14 @@
 (setq javascript-indent-level 2) ; javascript-mode
 (setq js-indent-level 2) ; js-mode
 (setq js2-basic-offset 2) ; js2-mode, in latest js2-mode, it's alias of js-indent-level
+;;(setq tide-basic-offset 2) ;
+(setq typescript-indent-level 2) ; 
 (setq web-mode-markup-indent-offset 2) ; web-mode, html tag in html file
 (setq web-mode-css-indent-offset 2) ; web-mode, css in html file
 (setq web-mode-code-indent-offset 2) ; web-mode, js code in html file
 (setq css-indent-offset 2) ; css-mode
+(setq typescript-indent-level 2
+      typescript-expr-indent-offset 2)
 
 (defun setup-tide-mode ()
   (interactive)
