6f7a4a6818c2ab78f6b2208ed209c360a60fb9a9
LS8 <lennartschoettker@hotmail.com>
2017-10-15 09:02:44 +0200

    add winum mode for window navigaten by spc-number to go to window-number

--- init.el
+++ init.el
@@ -240,6 +240,28 @@
   (add-hook 'typescript-mode-hook #'setup-tide-mode)
   )
 
+;;(use-package window-number :ensure t
+;;  :config
+;;  (setq window-number-mode t))
+;;(use-package window-numbering :ensure t)
+(use-package winum :ensure t
+  :config
+;;  (setq winum-keymap
+;;	(let ((map (make-sparse-keymap)))
+;;	  (define-key map (kbd "C-0") 'winum-select-window-0-or-10)
+;;	  (define-key map (kbd "C-1") 'winum-select-window-1)
+;;	  (define-key map (kbd "M-2") 'winum-select-window-2)
+;;	  (define-key map (kbd "M-3") 'winum-select-window-3)
+;;	  (define-key map (kbd "M-4") 'winum-select-window-4)
+;;	  (define-key map (kbd "M-5") 'winum-select-window-5)
+;;	  (define-key map (kbd "M-6") 'winum-select-window-6)
+;;	  (define-key map (kbd "M-7") 'winum-select-window-7)
+;;	  (define-key map (kbd "M-8") 'winum-select-window-8)
+;;	  map))
+  (winum-mode)
+  )
+
+
 (defun open-termite ()
   (interactive "@")
   (shell-command (concat "termite"
