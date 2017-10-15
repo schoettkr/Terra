06d1237b338bcd9647f2542e180d65420b8cab78
LS8 <lennartschoettker@hotmail.com>
2017-10-15 09:05:06 +0200

    add tabwidth of 2 to css, web and js mode

--- init.el
+++ init.el
@@ -289,3 +289,11 @@
 
 (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
 
+(setq javascript-indent-level 2) ; javascript-mode
+(setq js-indent-level 2) ; js-mode
+(setq js2-basic-offset 2) ; js2-mode, in latest js2-mode, it's alias of js-indent-level
+(setq web-mode-markup-indent-offset 2) ; web-mode, html tag in html file
+(setq web-mode-css-indent-offset 2) ; web-mode, css in html file
+(setq web-mode-code-indent-offset 2) ; web-mode, js code in html file
+(setq css-indent-offset 2) ; css-mode
+
