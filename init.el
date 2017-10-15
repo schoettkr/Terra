d4c26e41f9b6e31996d919660001d36118486562
LS8 <lennartschoettker@hotmail.com>
2017-10-15 08:47:18 +0200

    update

--- init.el
+++ init.el
@@ -41,7 +41,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (evil-magit projectile dashboard linum-relative arjen-grey-theme which-key evil avy general use-package))))
+    (winum window-numbering window-number tide typescript-mode typescript js2-mode company-tern yasnippet company evil-magit projectile dashboard linum-relative arjen-grey-theme which-key evil avy general use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
