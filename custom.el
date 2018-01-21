4cc011b98cd80d516bf4ab6547a31caecb036134
LS8 <lennartschoettker@hotmail.com>
2018-01-21 22:05:18 +0100

    Structure config into config.org and meaningful sections

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (ess zenburn-theme winum rainbow-delimiters polymode nord-theme neotree linum-relative indent-guide gruvbox-theme general frontmacs evil-magit evil-commentary dracula-theme doom-themes dashboard company-tern badger-theme arjen-grey-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
