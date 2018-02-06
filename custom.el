cb944b4f39c704690830d0da273a0835a7f6ed81
LS8 <lennartschoettker@hotmail.com>
2018-02-06 22:11:44 +0100

    Update

--- custom.el
+++ custom.el
@@ -3,9 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
  '(package-selected-packages
    (quote
-    (twillight-bright-theme tide typescript-mode js2-mode yasnippet company projectile counsel avy which-key evil ess zenburn-theme winum rainbow-delimiters polymode nord-theme neotree linum-relative indent-guide gruvbox-theme general frontmacs evil-magit evil-commentary dracula-theme doom-themes dashboard company-tern badger-theme arjen-grey-theme))))
+    (solarized-theme spacemacs-theme twillight-bright-theme tide typescript-mode js2-mode yasnippet company projectile counsel avy which-key evil ess zenburn-theme winum rainbow-delimiters polymode nord-theme neotree linum-relative indent-guide gruvbox-theme general frontmacs evil-magit evil-commentary dracula-theme doom-themes dashboard company-tern badger-theme arjen-grey-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
