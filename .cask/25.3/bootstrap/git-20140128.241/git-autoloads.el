686108b42f63dc44528cb7920a286d207087b47f
LS8 <lennartschoettker@hotmail.com>
2018-02-16 16:15:21 +0100

    update

---
+++ .cask/25.3/bootstrap/git-20140128.241/git-autoloads.el
@@ -0,0 +1,15 @@
+;;; git-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("git.el") (23174 55452 373237 710000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; git-autoloads.el ends here
