9e57344e5aa23fecbc8f909422371fc81dc650d8
LS8 <lennartschoettker@hotmail.com>
2018-04-21 21:25:18 +0200

    update

--- custom.el
+++ custom.el
@@ -4,9 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("bbb4a4d39ed6551f887b7a3b4b84d41a3377535ccccf901a3c08c7317fad7008" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default))
+   (quote
+    ("aa0a998c0aa672156f19a1e1a3fb212cdc10338fb50063332a0df1646eb5dfea" "891debfe489c769383717cc7d0020244a8d62ce6f076b2c42dd1465b7c94204d" "bbb4a4d39ed6551f887b7a3b4b84d41a3377535ccccf901a3c08c7317fad7008" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
  '(package-selected-packages
-   '(org-noter use-package tabbar org-bullets pdf-tools frame-cmds treemacs-evil treemacs-projectile treemacs ov ag company-go flymake-go go-guru go-mode solarized-theme spacemacs-theme twillight-bright-theme tide typescript-mode js2-mode yasnippet company projectile counsel avy which-key evil ess zenburn-theme winum rainbow-delimiters polymode nord-theme neotree linum-relative indent-guide gruvbox-theme general frontmacs evil-magit evil-commentary dracula-theme doom-themes dashboard company-tern badger-theme arjen-grey-theme)))
+   (quote
+    (highlight-indentation org-noter use-package tabbar org-bullets pdf-tools frame-cmds treemacs-evil treemacs-projectile treemacs ov ag company-go flymake-go go-guru go-mode solarized-theme spacemacs-theme twillight-bright-theme tide typescript-mode js2-mode yasnippet company projectile counsel avy which-key evil ess zenburn-theme winum rainbow-delimiters polymode nord-theme neotree linum-relative indent-guide gruvbox-theme general frontmacs evil-magit evil-commentary dracula-theme doom-themes dashboard company-tern badger-theme arjen-grey-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
