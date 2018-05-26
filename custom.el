7e0c201acb92e7ec816e242e6b829c8ca79597b8
LS8 <lennartschoettker@hotmail.com>
2018-05-26 10:01:01 +0200

    Add color identifiers mode!

--- custom.el
+++ custom.el
@@ -4,11 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "aa0a998c0aa672156f19a1e1a3fb212cdc10338fb50063332a0df1646eb5dfea" "891debfe489c769383717cc7d0020244a8d62ce6f076b2c42dd1465b7c94204d" "bbb4a4d39ed6551f887b7a3b4b84d41a3377535ccccf901a3c08c7317fad7008" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
+   '("013c62a1fcee7c8988c831027b1c38ae215f99722911b69e570f21fc19cb662e" "5715d3b4b071d33af95e9ded99a450aad674e308abb06442a094652a33507cd2" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "aa0a998c0aa672156f19a1e1a3fb212cdc10338fb50063332a0df1646eb5dfea" "891debfe489c769383717cc7d0020244a8d62ce6f076b2c42dd1465b7c94204d" "bbb4a4d39ed6551f887b7a3b4b84d41a3377535ccccf901a3c08c7317fad7008" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default))
  '(package-selected-packages
-   (quote
-    (markdown-mode beacon org-html-themes eyebrowse smooth-scrolling smooth-scroll diminish spaceline all-the-icons eldoc-eval smart-mode-line popup company-quickhelp highlight-indentation org-noter use-package tabbar org-bullets pdf-tools frame-cmds treemacs-evil treemacs-projectile treemacs ov ag company-go flymake-go go-guru go-mode solarized-theme spacemacs-theme twillight-bright-theme tide typescript-mode js2-mode yasnippet company projectile counsel avy which-key evil ess zenburn-theme winum rainbow-delimiters polymode nord-theme neotree linum-relative indent-guide gruvbox-theme general frontmacs evil-magit evil-commentary dracula-theme doom-themes dashboard company-tern badger-theme arjen-grey-theme))))
+   '(color-identifiers-mode markdown-mode beacon org-html-themes eyebrowse smooth-scrolling smooth-scroll diminish spaceline all-the-icons eldoc-eval smart-mode-line popup company-quickhelp highlight-indentation org-noter use-package tabbar org-bullets pdf-tools frame-cmds treemacs-evil treemacs-projectile treemacs ov ag company-go flymake-go go-guru go-mode solarized-theme spacemacs-theme twillight-bright-theme tide typescript-mode js2-mode yasnippet company projectile counsel avy which-key evil ess zenburn-theme winum rainbow-delimiters polymode nord-theme neotree linum-relative indent-guide gruvbox-theme general frontmacs evil-magit evil-commentary dracula-theme doom-themes dashboard company-tern badger-theme arjen-grey-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
