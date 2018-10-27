b4921757400de4f6ea817f985d7e9b5972dc0ba3
LS8 <lennartschoettker@hotmail.com>
2018-10-27 16:52:18 +0200

    Initialize package and deactivate GUI elements earlier

--- init.el
+++ init.el
@@ -3,10 +3,15 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-;;(package-initialize)
+
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(toggle-scroll-bar -1)
+(package-initialize)
+
 
 (require 'org)
 (let ((file-name-handler-alist nil))
-(org-babel-load-file
- (expand-file-name "config.org"
-		   user-emacs-directory)))
+  (org-babel-load-file
+   (expand-file-name "config.org"
+                     user-emacs-directory)))
