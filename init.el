7cdd6fee0c793a43be71e7c50ad410a2242c7af6
LS8 <lennartschoettker@hotmail.com>
2019-07-22 00:02:18 +0200

    Add more minimalistic emacs config which is also pretty fast

--- init.el
+++ init.el
@@ -3,12 +3,13 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-
+(setq gc-cons-threshold 100000000)
+(let ((file-name-handler-alist nil))
 (menu-bar-mode 0)
 (tool-bar-mode 0)
 (toggle-scroll-bar -1)
 (package-initialize)
-
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 (unless (file-exists-p "~/.emacs.d/custom.el")
   (with-temp-buffer (write-file "~/.emacs.d/custom.el")))
 
@@ -17,3 +18,4 @@
   (org-babel-load-file
    (expand-file-name "config.org"
                      user-emacs-directory)))
+)
